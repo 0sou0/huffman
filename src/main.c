1bc6e1f6aff57d73ba8334546ef0d11e2217e04b
sousou <kherbacheyacine@kherbaches-macbook-air.home>
2024-05-26 21:48:24 +0200

    feat: v5

--- src/main.c
+++ src/main.c
@@ -4,46 +4,48 @@
 #include "decoder.h"
 #include "encoder.h"
 #include "outils.h"
+#include <sys/stat.h>
 
 
 
 
 /****************************************
  * fonction : main
- * description : Exécute le processus de compression de Huffman, puis décompresse pour vérification
+ * description : Point d'entrée principal du programme. Gère les arguments de la ligne de commande
+ *               pour effectuer la compression ou la décompression selon l'option choisie.
+ *               Initialise les structures de données nécessaires, appelle les fonctions de 
+ *               compression ou de décompression, et gère les ressources et la fermeture des fichiers.
  * entree : entiers argc (nombre d'arguments), tableau de chaînes argv (arguments)
  * sortie : (int) état de sortie du programme
  ****************************************/
 int main(int argc, char *argv[]){
-  FILE *fichier_compresse=NULL; 
+  FILE *fichier_compresse=NULL;
   noeud *arbre_huffman[N];
-  noeud *alphabet[N]; 
-  int  nb_noeuds=0, nombre_feuilles=0;
-  int opt, compresse = 0, decompresse = 0, optind=2;
+  noeud *alphabet[N];
   char *chemin_fichier_compresse = NULL;
-  char *chemin_dossier = NULL;
-  
-
+  int i, nb_noeuds=0, nombre_feuilles=0;
+  int compresse = 0, decompresse = 0;
+  int opt, optind=2;
   int tab_global[N];
-
-
-/*Validation du nombre d'arguments*/
-    if(argc < 3){
-      usage(argv[0]);
-      exit(EXIT_FAILURE);
-    }
+  struct stat info;
   
+ 
+  
+/*Validation du nombre d'arguments*/
+  if(argc < 2){
+    usage(argv[0]);
+    exit(EXIT_FAILURE);
+  }
+/*Traitement des arguments de la ligne de commande*/
   while ( (opt=getopt(argc, argv, "c:d:h")) != -1){
         switch (opt){
         case 'c':
           compresse = 1;
-          
           break;
         case 'd':
           decompresse = 1;
-          
           break;
-      	case 'h':
+        case 'h':
           afficher_aide();
           exit(EXIT_FAILURE);
           break;
@@ -53,68 +55,69 @@ int main(int argc, char *argv[]){
         }
   }
 
-
 /*Initialisation de l'arbre Huffman et de l'alphabet*/
 initialiser_arbre_huffman(arbre_huffman);
 initialiser_alphabet(alphabet);
-   
+
+/*Processus de compression*/
 if(compresse == 1){
   init_tab(tab_global);
 
-   chemin_dossier = argv[optind];
-   chemin_fichier_compresse = argv[optind + 1];
-      
-  
-init_tab(tab_global);
-compression_rec(chemin_dossier, chemin_fichier_compresse, fichier_compresse, alphabet, tab_global);
+  if (optind < argc) {
+    chemin_fichier_compresse = argv[argc - 1];
+  }
+ 
+  for (i = optind; i < argc - 1; i++) {
+        compression_rec(argv[i],chemin_fichier_compresse, fichier_compresse, alphabet, tab_global);
+    }
+ 
 arbre(arbre_huffman, tab_global);
 creer_feuilles_huffman(arbre_huffman, tab_global, &nb_noeuds);
-affichage_car(arbre_huffman);
+/*affichage_car(arbre_huffman);*/
 construire_arbre_huffman(arbre_huffman, &nb_noeuds);
 creer_code(arbre_huffman[0], 0, 0, alphabet);
-nombre_feuilles = compter_nombre_feuilles(alphabet);  
+nombre_feuilles = compter_nombre_feuilles(alphabet);
 
- 
 
 fichier_compresse = fopen(chemin_fichier_compresse, "wb");
-        if (fichier_compresse == NULL) {
-            printf("Erreur lors de l'ouverture du fichier compressé");
-            exit(EXIT_FAILURE);
-        }
-      ecrire_entete(fichier_compresse, alphabet, nombre_feuilles);
-      contenu_rec(chemin_dossier, chemin_fichier_compresse, fichier_compresse, alphabet);
-    }
-    
-  
-  
-  if(decompresse == 1){ 
-  if (optind < argc) {
-            chemin_fichier_compresse = argv[optind++];
-        } 
-    fichier_compresse = fopen(chemin_fichier_compresse, "rb");
-    if (fichier_compresse == NULL) {
-        printf("Erreur lors de l'ouverture du fichier compressé pour lecture");
+  if (fichier_compresse == NULL) {
+      printf("Erreur lors de l'ouverture du fichier compressé");
         exit(EXIT_FAILURE);
+  }
+
+  ecrire_entete(fichier_compresse, alphabet, nombre_feuilles);
+  for ( i = optind; i < argc - 1; i++) {
+    contenu_rec(argv[i],chemin_fichier_compresse, fichier_compresse, alphabet);
+    if (stat(argv[i], &info) != 0) {
+        perror("Erreur on arrive pas à savoir si le chemin amene vers un fichier ou un dossier");
+        exit(EXIT_FAILURE);
+    }
+    if(!S_ISDIR(info.st_mode)){
+      printf("fichier : %s \n", argv[i]);
+      printf("taille fichier : %d\n",taille_fichier((argv[i])));
+    }
+    }
+    fclose(fichier_compresse);
+ }
+
+
+/*Processus de décompression*/ 
+if(decompresse == 1){ 
+  if (optind < argc) {
+    chemin_fichier_compresse = argv[argc - 1];
+  } 
+  fichier_compresse = fopen(chemin_fichier_compresse, "rb");
+    if (fichier_compresse == NULL) {
+      printf("Erreur ouverture du fichier compressé");
+      exit(EXIT_FAILURE);
     }
   
   
   lire_entete(fichier_compresse, alphabet);
   lire_contenu(fichier_compresse, alphabet);
 }
- 
+  
 /*Libération des ressources et fermeture des fichiers*/
   liberer_inis(arbre_huffman);
-
   exit(EXIT_SUCCESS);
-
-
 }
-
-
-
-
-
-
-
-
-
