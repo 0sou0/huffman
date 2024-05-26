6ad43585acfefc0bdf2b660ca6999bc3f88129f0
Kherbache Yacine <kherbacheyacine@kherbaches-macbook-air.home>
2024-05-26 21:42:49 +0200

    feat: v4

--- src/main.c
+++ src/main.c
@@ -4,7 +4,6 @@
 #include "decoder.h"
 #include "encoder.h"
 #include "outils.h"
-#define PATH_MAX 4096
 
 
 
@@ -16,31 +15,33 @@
  * sortie : (int) état de sortie du programme
  ****************************************/
 int main(int argc, char *argv[]){
-  struct dirent* fichierLu = NULL;
-  FILE * fichier = NULL;
-  FILE *fichier_compresse=NULL;
-  DIR *dossier = NULL;
+  FILE *fichier_compresse=NULL; 
   noeud *arbre_huffman[N];
-  noeud *alphabet[N];
-
-  
-  int i, nb_noeuds=0, nombre_feuilles=0;
+  noeud *alphabet[N]; 
+  int  nb_noeuds=0, nombre_feuilles=0;
   int opt, compresse = 0, decompresse = 0, optind=2;
   char *chemin_fichier_compresse = NULL;
   char *chemin_dossier = NULL;
-  char nom_fichier[PATH_MAX];
+  
 
   int tab_global[N];
+
+
+/*Validation du nombre d'arguments*/
+    if(argc < 3){
+      usage(argv[0]);
+      exit(EXIT_FAILURE);
+    }
   
   while ( (opt=getopt(argc, argv, "c:d:h")) != -1){
         switch (opt){
         case 'c':
           compresse = 1;
-          chemin_dossier = argv[optind];
+          
           break;
         case 'd':
           decompresse = 1;
-          chemin_fichier_compresse = argv[optind];
+          
           break;
       	case 'h':
           afficher_aide();
@@ -53,123 +54,46 @@ int main(int argc, char *argv[]){
   }
 
 
-
-  
-    /*Initialisation de l'arbre Huffman et de l'alphabet*/
-    for(i = 0; i < 256; i++){
-      arbre_huffman[i] = (noeud*)malloc(sizeof(noeud));
-      if(arbre_huffman[i] == NULL){
-        printf("Erreur de l'allocation memoire\n");
-        exit(EXIT_FAILURE);
-      }
-    }
-        
-    for(i = 0; i < 256; i++){
-      alphabet[i] = NULL; 
-    }
-
-
-
-  /*Validation du nombre d'arguments*/
-    if(argc < 3){
-      usage(argv[0]);
-      exit(EXIT_FAILURE);
-    }
-
+/*Initialisation de l'arbre Huffman et de l'alphabet*/
+initialiser_arbre_huffman(arbre_huffman);
+initialiser_alphabet(alphabet);
+   
 if(compresse == 1){
   init_tab(tab_global);
-  dossier = opendir(chemin_dossier);
-  if(dossier == NULL){
-    perror("Erreur ouverture dossier \n");
-    exit(EXIT_FAILURE);
-  }
-      while((fichierLu = readdir(dossier))!= NULL){
-        if(strcmp(fichierLu->d_name, ".") == 0 || strcmp(fichierLu->d_name, "..") == 0){
-          continue;
-        }
-        sprintf(nom_fichier, "%s/%s",chemin_dossier, fichierLu->d_name);
-            fichier = fopen(nom_fichier, "rb");
-            if (fichier == NULL) {
-                fprintf(stderr, "Erreur lors de l'ouverture du fichier %s\n", fichierLu->d_name);
-                continue;
-            }
- 
-    
-    occurence(fichier, tab_global);
-    fclose(fichier);
-  }
 
-
-    arbre(arbre_huffman, tab_global);
-    
-
-  /*Création des feuilles de l'arbre Huffman*/
-    for (i = 0; i < 256; i++) {
-        if (tab_global[i] > 0) {
-        arbre_huffman[nb_noeuds] = creer_feuille(tab_global, i);
-        nb_noeuds++; 
-      }
-    }
-
-    affichage_car(arbre_huffman);
-
-  /* Construction de l'arbre de Huffman complet*/
-    while (nb_noeuds > 1) {  
-      creer_noeud(arbre_huffman, nb_noeuds);
-      nb_noeuds--; 
-    }
-   
-
-  /*création des codes de Huffman pour chaque caractère*/
-    creer_code(arbre_huffman[0], 0, 0, alphabet);
-    printf("-------------------------------------\n");
-  	
-  /*Comptage des feuilles utilisées dans l'arbre Huffman*/
-    for (i = 0; i < 256; i++) {
-      if (alphabet[i] != NULL) {
-        nombre_feuilles++;
-      }
-    }
-
+   chemin_dossier = argv[optind];
+   chemin_fichier_compresse = argv[optind + 1];
+      
   
+init_tab(tab_global);
+compression_rec(chemin_dossier, chemin_fichier_compresse, fichier_compresse, alphabet, tab_global);
+arbre(arbre_huffman, tab_global);
+creer_feuilles_huffman(arbre_huffman, tab_global, &nb_noeuds);
+affichage_car(arbre_huffman);
+construire_arbre_huffman(arbre_huffman, &nb_noeuds);
+creer_code(arbre_huffman[0], 0, 0, alphabet);
+nombre_feuilles = compter_nombre_feuilles(alphabet);  
 
+ 
 
-
-
-    fichier_compresse = fopen(argv[argc - 1], "wb");
-      if (fichier_compresse == NULL) {
-          perror("Erreur lors de l'ouverture du fichier compressé pour écriture");
-          exit(EXIT_FAILURE);
-      }
-
-      ecrire_entete(fichier_compresse, alphabet, nombre_feuilles);
-      rewinddir(dossier);
-      while((fichierLu = readdir(dossier))!= NULL){
-        if(strcmp(fichierLu->d_name, ".") == 0 || strcmp(fichierLu->d_name, "..") == 0){
-          continue;
+fichier_compresse = fopen(chemin_fichier_compresse, "wb");
+        if (fichier_compresse == NULL) {
+            printf("Erreur lors de l'ouverture du fichier compressé");
+            exit(EXIT_FAILURE);
         }
-        sprintf(nom_fichier, "%s/%s",chemin_dossier, fichierLu->d_name);
-            fichier = fopen(nom_fichier, "rb");
-            if (fichier == NULL) {
-                fprintf(stderr, "Erreur lors de l'ouverture du fichier %s\n", fichierLu->d_name);
-                continue;
-            }
-      fichier = fopen(nom_fichier, "rb");
-      fprintf(fichier_compresse, "%s ",nom_fichier );
-      contenu_compresse(fichier, fichier_compresse, alphabet);
-      fclose(fichier);
+      ecrire_entete(fichier_compresse, alphabet, nombre_feuilles);
+      contenu_rec(chemin_dossier, chemin_fichier_compresse, fichier_compresse, alphabet);
     }
     
-
-    fclose(fichier_compresse);
-    closedir(dossier);
-  }
+  
   
   if(decompresse == 1){ 
-
+  if (optind < argc) {
+            chemin_fichier_compresse = argv[optind++];
+        } 
     fichier_compresse = fopen(chemin_fichier_compresse, "rb");
     if (fichier_compresse == NULL) {
-        perror("Erreur lors de l'ouverture du fichier compressé pour lecture");
+        printf("Erreur lors de l'ouverture du fichier compressé pour lecture");
         exit(EXIT_FAILURE);
     }
   
@@ -177,7 +101,7 @@ if(compresse == 1){
   lire_entete(fichier_compresse, alphabet);
   lire_contenu(fichier_compresse, alphabet);
 }
-
+ 
 /*Libération des ressources et fermeture des fichiers*/
   liberer_inis(arbre_huffman);
 
