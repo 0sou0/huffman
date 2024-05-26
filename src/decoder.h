2a3cc882598a83324f44a1c9ee0ce5e708f56764
sousou <sousou@for-my-alma.fr>
2024-05-26 22:05:11 +0200

    feat: v1

---
+++ src/decoder.h
@@ -0,0 +1,24 @@
+
+#ifndef DECODER_H_
+#define DECODER_H_
+
+
+
+/****************************************
+ * fonction : lire_entete
+ * description : Lit l'en-tête du fichier compressé pour extraire l'alphabet utilisé, 
+ *               avec les occurrences, les bits utilisés pour chaque caractère, et leur codage
+ * entree : pointeur vers fichier FILE *fichier, tableau de pointeurs vers noeud alphabet[]
+ * sortie : aucune (void), termine le programme en cas d'erreur de lecture
+ ****************************************/
+void lire_entete(FILE *fichier, noeud *alphabet[]);
+
+/****************************************
+ * fonction : lire_contenu
+ * description : Lit le contenu compressé, décode les caractères selon l'alphabet et les écrit dans un fichier de sortie
+ * entree : pointeur vers fichier FILE *fichier_compresse, tableau de pointeurs vers noeud alphabet[]
+ * sortie : aucune (void), crée un fichier "decompresse.txt" contenant le texte décompressé
+ ****************************************/
+void lire_contenu(FILE *fichier_compresse, noeud *alphabet[],char* chemin);
+
+#endif 
